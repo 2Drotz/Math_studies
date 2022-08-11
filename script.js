39927e3ae1b552c1610870f8f17232f997e02ea1
Dmitry <mandrin2013@mail.ru>
2022-08-11 19:35:21 +0300

    commit

--- script.js
+++ script.js
@@ -1,63 +1,115 @@
-const option1 = document.querySelector('#option1'),
-   option2 = document.querySelector('#option2'),
-   audioYes = document.querySelector('#yes'),
+const audioYes = document.querySelector('#yes'),
    audioNo = document.querySelector('#no'),
-   option3 = document.querySelector('#option3');
+   option = document.querySelectorAll('#option'),
+   znak = document.querySelector('#znak'),
+   btnLink = document.querySelectorAll('.btn'),
+   done = document.querySelector('#done'),
+   fail = document.querySelector('#fail');
 
-let answer = 0;
+let answer = 0,
+   doneAnimation,
+   failAnimation;
 
-function generate_equation() {
-   let num1 = Math.floor(Math.random() * 20);
-   let num2 = Math.floor(Math.random() * 22);
+function generateEquation() {
+   let num1 = Math.floor(Math.random() * 10),
+      num2 = Math.floor(Math.random() * 12);
    answer = num1 + num2;
 
-   let wrongAnswer1 = answer + 1;
-   let wrongAnswer2 = answer - 1;
+   let wrongAnswer1 = answer + 1,
+      wrongAnswer2 = answer - 1;
 
    document.querySelector('#num1').innerHTML = num1;
    document.querySelector('#num2').innerHTML = num2;
-
 
    let allAnswer = [answer, wrongAnswer1, wrongAnswer2];
    allAnswer.sort(function () {
       return Math.floor(Math.random(allAnswer) - 0.5);
    });
 
-
-
-   option1.innerHTML = allAnswer[0];
-   option2.innerHTML = allAnswer[1];
-   option3.innerHTML = allAnswer[2];
+   option.forEach((item, i) => {
+      item.innerHTML = allAnswer[i];
+   });
 }
-option1.addEventListener('click', () => {
-   if (option1.innerHTML == answer) {
-      generate_equation();
-      audioYes.play();
+generateEquation();
 
-   } else {
-      audioNo.play();
-   }
-});
+option.forEach((item) => {
 
-option2.addEventListener('click', () => {
-   if (option2.innerHTML == answer) {
-      generate_equation();
-      audioYes.play();
-   } else {
-      audioNo.play();
-   }
-});
+   item.addEventListener('click', () => {
+      if (item.innerHTML == answer) {
+         generateEquation();
+         audioYes.play();
+         doneCounter();
 
-option3.addEventListener('click', () => {
-   if (option3.innerHTML == answer) {
-      generate_equation();
-      audioYes.play();
-   } else {
-      audioNo.play();
-   }
+      } else {
+         audioNo.play();
+         failCounter();
+      }
+   });
 });
 
 
-generate_equation();
 
-console.log(Math.floor(Math.random() - 0.5));
+function doneCounter() {
+   let doneCount = done.textContent;
+   doneCount++;
+   animate(doneAnimation, done);
+   done.textContent = doneCount;
+
+   if (doneCount >= 5) {
+      alert('Молодец ты проходишь на следующий уровень!');
+      done.textContent = 0;
+      fail.textContent = 0;
+   }
+}
+function failCounter() {
+   let failCount = fail.textContent;
+   failCount++;
+   fail.textContent = failCount;
+   animate(failAnimation, fail);
+
+   if (failCount == 3) {
+      alert('Ты проиграл!');
+      fail.textContent = 0;
+   }
+}
+
+
+
+
+// btnLink.forEach((item) => {
+
+//    item.addEventListener('click', () => {
+//       if (item.textContent == 'Вычитание') {
+//          znak.textContent = '-';
+//          generateEquation();
+//       }
+//    });
+// });
+
+let scale = [
+   {
+      filter: 'opacity(100%)',
+   },
+   {
+      filter: 'opacity(0%)',
+   },
+   {
+      filter: 'opacity(100%)',
+   }
+];
+
+
+let time = {
+   duration: 250,
+   iterations: 1
+};
+
+
+animate = (animate, item) => {
+   if (!animate) {
+      animate = item.animate(scale, time);
+
+   } else {
+      animate.play();
+   }
+};
